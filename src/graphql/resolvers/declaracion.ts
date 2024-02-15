2fd8e47553fc15814969d23e1d9e44d45831ec83
Benito Perdomo <benito.perdomo.v@gmail.com>
2024-02-15 15:30:14 -0600

    Format

--- src/graphql/resolvers/declaracion.ts
+++ src/graphql/resolvers/declaracion.ts
@@ -1,27 +1,14 @@
-import {
-  Context,
-  Declaracion,
-  DeclaracionDocument,
-  DeclaracionSecciones,
-  DeclaracionesFilterInput,
-  Pagination,
-  PaginationInputOptions,
-  TipoDeclaracion,
-} from '../../types';
+import { Context, Declaracion, DeclaracionDocument, DeclaracionSecciones, DeclaracionesFilterInput, Pagination, PaginationInputOptions, TipoDeclaracion } from '../../types';
 import CreateError from 'http-errors';
 import { DeclaracionRepository } from '../../db/repositories/declaracion_repo';
 
 export default {
   Query: {
-    declaracion(
-      _: unknown,
-      args: { tipoDeclaracion: TipoDeclaracion, declaracionCompleta?: boolean },
-      context: Context,
-    ): Promise<Declaracion> {
+    declaracion(_: unknown, args: { tipoDeclaracion: TipoDeclaracion; declaracionCompleta?: boolean }, context: Context): Promise<Declaracion> {
       return DeclaracionRepository.getOrCreate(context.user.id, args.tipoDeclaracion, args.declaracionCompleta);
     },
 
-    declaracionesMetadata(_: unknown, args: { userID?: string, filter?: DeclaracionesFilterInput, pagination?: PaginationInputOptions }, context: Context): Promise<Pagination<DeclaracionDocument>> {
+    declaracionesMetadata(_: unknown, args: { userID?: string; filter?: DeclaracionesFilterInput; pagination?: PaginationInputOptions }, context: Context): Promise<Pagination<DeclaracionDocument>> {
       const scopes = context.user.scopes;
       if (args.userID) {
         if (scopes.includes('DeclarationMetada:read:all')) {
@@ -40,18 +27,18 @@ export default {
       }
 
       throw new CreateError.Unauthorized(`User[${context.user.id}] is not allowed to perform this operation.`);
-    },
+    }
   },
 
   Mutation: {
-    declaracion(_: unknown, args: { id: string, declaracion: DeclaracionSecciones }, context: Context): Promise<Declaracion> {
+    declaracion(_: unknown, args: { id: string; declaracion: DeclaracionSecciones }, context: Context): Promise<Declaracion> {
       return DeclaracionRepository.update(args.id, context.user.id, args.declaracion);
     },
     deleteDeclaracion(_: unknown, args: { id: string }, context: Context): Promise<boolean> {
       return DeclaracionRepository.delete(args.id, context.user.id);
     },
-    firmarDeclaracion(_: unknown, args: { id: string, password: string }, context: Context): Promise<Record<string, any> | null> {
+    firmarDeclaracion(_: unknown, args: { id: string; password: string }, context: Context): Promise<Record<string, any> | null> {
       return DeclaracionRepository.sign(args.id, args.password, context.user.id);
-    },
+    }
   }
 };
